5942b2da2c415495eb8ebd68c5f3cffc66eec1fa
Felipe Maciel <felipemrvieira@gmail.com>
2019-02-25 13:52:29 -0300

    Cria menu

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.scss';
 
 import Header from './components/template/Header';
-
+import Nav from './components/template/Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Nav />
       </div>
     );
   }
