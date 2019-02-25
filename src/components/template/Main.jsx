4b05de06afa2f9f9914a2a7c347d187129f082f4
Felipe Maciel <felipemrvieira@gmail.com>
2019-02-25 15:31:05 -0300

    Cria o componente orgaos defesa

--- src/components/template/Main.jsx
+++ src/components/template/Main.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import BannerPrincipal from './BannerPrincipal';
+import OrgaosDefesa from './OrgaosDefesa';
 
 class Main extends Component {
   render() {
     return (
         <main>
             <BannerPrincipal />
+            <OrgaosDefesa />
         </main>
     );
   }
