3a1ba6579c9ce3da43b2ef474faa714d35f1390c
Felipe Maciel Ramos Vieira <felipemrvieira@gmail.com>
2019-02-28 22:30:48 -0300

    Integra banner da api

--- src/components/template/Main/Main.jsx
+++ src/components/template/Main/Main.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import TelefonesUteis from './TelefonesUteis';
 import BannerContingencia from './BannerContingencia';
@@ -10,30 +11,46 @@ import AtuacaoIntegrada from './AtuacaoIntegrada';
 import BannerPrincipal from './BannerPrincipal';
 import OrgaosDefesa from './OrgaosDefesa';
 
+const URL = 'http://api.itec.al.gov.br/api/v1/banners';
+
 
 class Main extends Component {
-  render() {
-    return (
-        <main>
-            <BannerPrincipal />
-            <OrgaosDefesa />
-            <TelefonesUteis />
-            <BannerContingencia/>
-            <section id="mapa-noticias" className="container">
-              <div className="row">
-                <div className="col-lg-6">
-                  <MapaInstabilidade />
+  constructor(props) {
+    super(props)
+    this.state = { banner: {} }
+    this.refresh()
+  }
+  refresh() {
+    axios.get(`${URL}`)
+        .then(resp => {
+          this.setState({ ...this.state, banner: resp.data[0] })
+        })
+        .catch( err => console.log(err) )
+      }
+      
+    render() {
+      const img = this.state.banner.imagem;
+      return (
+          <main>
+              <BannerPrincipal banner={img}/>
+              <OrgaosDefesa />
+              <TelefonesUteis />
+              <BannerContingencia/>
+              <section id="mapa-noticias" className="container">
+                <div className="row">
+                  <div className="col-lg-6">
+                    <MapaInstabilidade />
+                  </div>
+                  <div className="col-lg-6">
+                    <Noticias />
+                  </div>
                 </div>
-                <div className="col-lg-6">
-                  <Noticias />
-                </div>
-              </div>
-            </section>
-            <AreasAtingidas />
-            <BannersDiversos />
-            <AtuacaoIntegrada />
-        </main>
-    );
+              </section>
+              <AreasAtingidas />
+              <BannersDiversos />
+              <AtuacaoIntegrada />
+          </main>
+      );
   }
 }
 
